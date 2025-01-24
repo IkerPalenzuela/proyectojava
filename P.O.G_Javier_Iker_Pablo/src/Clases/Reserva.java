1ad1b0f9509adf14951fe802a6634143077056b1
Iker Palenzuela <ikdgr@plaiaundi.net>
2025-01-24 12:58:17 +0100

    commit reserva

--- P.O.G_Javier_Iker_Pablo/src/Clases/Reserva.java
+++ P.O.G_Javier_Iker_Pablo/src/Clases/Reserva.java
@@ -2,4 +2,61 @@ package Clases;
 
 public class Reserva {
 
+	private int idReserva;
+	private String dni;
+	private String codAvion;
+	private String fechaIda;
+	private String fechaVuelta;
+	
+	// Constructor
+	public Reserva(int idReserva, String dni, String codAvion, String fechaIda, String fechaVuelta) {
+		this.idReserva = idReserva;
+		this.dni = dni;
+		this.codAvion = codAvion;
+		this.fechaIda = fechaIda;
+		this.fechaVuelta = fechaVuelta;
+	}
+
+	// Getters & Setters
+	public int getIdReserva() {
+		return idReserva;
+	}
+
+	public void setIdReserva(int idReserva) {
+		this.idReserva = idReserva;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getCodAvion() {
+		return codAvion;
+	}
+
+	public void setCodAvion(String codAvion) {
+		this.codAvion = codAvion;
+	}
+
+	public String getFechaIda() {
+		return fechaIda;
+	}
+
+	public void setFechaIda(String fechaIda) {
+		this.fechaIda = fechaIda;
+	}
+
+	public String getFechaVuelta() {
+		return fechaVuelta;
+	}
+
+	public void setFechaVuelta(String fechaVuelta) {
+		this.fechaVuelta = fechaVuelta;
+	}
+	
+	
 }
