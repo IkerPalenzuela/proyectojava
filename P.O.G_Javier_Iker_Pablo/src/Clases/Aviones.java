88a96f0c6b8c8fca56f5838fea5f7add46e2e3d2
Iker Palenzuela <ikdgr@plaiaundi.net>
2025-01-27 09:33:00 +0100

    commit clase aviones

--- P.O.G_Javier_Iker_Pablo/src/Clases/Aviones.java
+++ P.O.G_Javier_Iker_Pablo/src/Clases/Aviones.java
@@ -1,6 +1,81 @@
 package Clases;
 
+import java.util.ArrayList;
+
 public class Aviones {
 
-	//Holahghgh
+	// Atributos
+	protected String fabricante;
+	protected String  modelo;
+	protected int millas;
+	protected ArrayList<Reserva> reserva;
+	protected Hangar hangar;
+	
+	// Constructor 
+	public Aviones(String fabricante, String modelo, int millas) {
+		this.fabricante = fabricante;
+		this.modelo = modelo;
+		this.millas = millas;
+		this.reserva = new ArrayList <>();
+		this.hangar = null;	
+	}
+	
+	// Metodos
+	public void agregarReserva(Reserva reserva) {
+		reserva.add(reserva);
+	}
+	
+	public void eliminarReserva(Reserva reserva) {
+		reserva.remove(reserva);
+	}
+	
+	public ArrayList<Reserva> obtenerReservas(){
+		return reserva;
+	}
+	
+	 public void asignarHangar(Hangar hangar) {
+		 this.hangar = hangar;
+	 }
+
+	 // Getters & Setters
+	 public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getMillas() {
+		return millas;
+	}
+
+	public void setMillas(int millas) {
+		this.millas = millas;
+	}
+
+	public Hangar getHangar() {
+		return hangar;
+	}
+
+	public void setHangar(Hangar hangar) {
+		this.hangar = hangar;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Aviones [fabricante=" + fabricante + ", modelo=" + modelo + ", millas=" + millas + ", hangar=" + hangar
+				+ "]";
+	}
+		
+	
 }
